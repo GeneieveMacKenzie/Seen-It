3617778fb944f99532583bb074b5139d73360846
Jeremy O <galaxyjay123@gmail.com>
2019-09-13 15:15:07 -0600

    posts inital fixed v-for

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <h1>Welcome to SocialSite.com!</h1>
+    <div class="container">
+      <div class="row">
+
+        <h1>Seen It</h1>
+
+
+      </div>
+      <div class="row">
+        <HomeComp v-for="home in homeComp" :homeCompProp="home" :key="home._id" />
+        <div class="card" style="width: 18rem;">
+
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
+
 </template>
 
 <script>
-export default {
-  name: "Home",
-  data() {
-    return {};
-  },
-  computed: {},
-  methods: {}
-};
+  import HomeComp from '@/components/HomeComp.vue'
+  export default {
+    name: "home",
+    components: {
+      HomeComp
+    },
+    data() {
+      return {};
+    },
+    mounted() {
+
+      this.$store.dispatch("getHomeComp")
+    },
+    computed: {
+      homeComp() {
+        return this.$store.state.homeComp;
+      }
+    },
+    methods: {}
+  };
 </script>
 
 <style>
