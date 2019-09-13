85d214b3813dcfec5b6f9396fc79e1465b901010
Andrew Gruver <gr82cu@gmail.com>
2019-09-13 10:54:47 -0600

    CommentController layout

---
+++ server/controllers/CommentController.js
@@ -0,0 +1,69 @@
+import express from 'express'
+import CommentService from '../services/CommentService'
+import { Authorize } from '../middleware/authorize.js'
+
+let _commentService = new CommentService().repository
+
+
+export default class CommentController {
+  constructor() {
+    this.router = express.Router()
+      .use('', function (req, res, next) {
+        console.log('In Comment: ', 'Request URL:', req.originalUrl, 'Request Type:', req.method, 'Request Author:', req.session.uid)
+        next()
+      })
+      .get('/:id', this.getAllByPostId)
+      .use(Authorize.authenticated)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
+  }
+
+  async getAllByPostId(req, res, next) {
+    try {
+      // console.log('getById blog', req.params.id, req.session.uid, req.originalUrl, req.method)
+      let data = await _commentService.findById(req.params.id).populate("authorId", "name")
+      if (data) {
+        return res.send(data)
+      }
+      throw new Error("Invalid Id")
+    } catch (error) { next(error) }
+  }
+
+  async create(req, res, next) {
+    try {
+      // console.log('create blog', req.session.uid, req.originalUrl, req.method)
+      console.log("THE BODY", req.body)
+      req.body.author = req.session.uid
+      let data = await _commentService.create(req.body)
+      if (data) {
+        return res.send(data)
+      }
+      throw new Error("Failed create!")
+    } catch (error) { next(error) }
+  }
+
+  async edit(req, res, next) {
+    try {
+      // console.log('edit blog', req.params.id, req.session.uid, req.originalUrl, req.method)
+      let data = await _commentService.findOneAndUpdate({ _id: req.params.id, authorId: req.session.uid }, req.body, { new: true }).populate("authorId", "name")
+      if (data) {
+        return res.send(data)
+      }
+      throw new Error("Invalid Id")
+    } catch (error) { next(error) }
+  }
+
+
+  async delete(req, res, next) {
+    try {
+      // console.log('delete blog ', req.params.id, req.session.uid, req.originalUrl, req.method)
+      let data = await _commentService.findOneAndDelete({ _id: req.params.id, authorId: req.session.uid })
+      if (data) {
+        return res.send("Deleted Blog")
+      }
+      // console.log('delete ', req.params.id)
+      throw new Error("Invalid Id")
+    } catch (error) { next(error) }
+  }
+}
--- server/services/CommentService.js
+++ server/services/CommentService.js
@@ -3,14 +3,13 @@ const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
 const _model = new Schema({
-  title: { type: String, required: true },
+  postId: { type: ObjectId, ref: 'post', required: true },,
   authorId: { type: ObjectId, ref: "User", required: true },
   body: { type: String, required: true }
-  //NOTE will eventually need movie id from api
 }, { timestamps: true })
 
-export default class PostService {
+export default class CommentService {
   get repository() {
-    return mongoose.model('post', _model)
+    return mongoose.model('comment', _model)
   }
 }
