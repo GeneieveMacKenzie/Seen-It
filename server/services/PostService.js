bccd04436e8bf777b1e25fd3a23923892e559369
Andrew Gruver <gr82cu@gmail.com>
2019-09-13 15:36:08 -0600

    New Post schema

--- server/services/PostService.js
+++ server/services/PostService.js
@@ -4,6 +4,11 @@ const ObjectId = Schema.Types.ObjectId
 
 const _model = new Schema({
   title: { type: String, required: true },
+  poster_path: { type: String, required: true },
+  release_date: { type: Date },  // check type
+  genre_ids: { type: String }, // check type 
+  id: { type: String, required: true }, // this is the movie id
+  postTitle: { type: String, required: true },
   authorId: { type: ObjectId, ref: "User", required: true },
   body: { type: String, required: true }
   //NOTE will eventually need movie id from api
