372e36a377929da22577755056fcea131ea39d12
Andrew Gruver <gr82cu@gmail.com>
2019-09-13 17:08:02 -0600

    Homepage user info

---
+++ client/src/components/PersonalInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="personal-info">
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkn0wxT8chcHq4SD_RHbcOe_o2slPsbzdxpGsFaKS4rI6nhCfe"
+      class="img-fluid"
+    />
+    <h1>{{user.name}}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "personal-info",
+  props: ["user"],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,51 +2,57 @@
   <div class="home">
     <div class="container">
       <div class="row">
-
         <h1>Seen It</h1>
-
-
       </div>
       <div class="row">
-        <HomeComp v-for="home in homeComp" :homeCompProp="home" :key="home._id" />
-        <div class="card" style="width: 18rem;">
-
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="col-3">
+          <PersonalInfo :user="user" />
+        </div>
+        <div class="col-6">
+          <HomeComp v-for="home in homeComp" :homeCompProp="home" :key="home._id" />
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card's
+                content.
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
   </div>
-
 </template>
 
 <script>
-  import HomeComp from '@/components/HomeComp.vue'
-  export default {
-    name: "home",
-    components: {
-      HomeComp
-    },
-    data() {
-      return {};
-    },
-    mounted() {
+import HomeComp from "@/components/HomeComp.vue";
+import PersonalInfo from "@/components/PersonalInfo.vue";
 
-      this.$store.dispatch("getHomeComp")
+export default {
+  name: "home",
+  components: {
+    HomeComp,
+    PersonalInfo
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    this.$store.dispatch("getHomeComp");
+    // this.$store.dispatch("getPersonalInfo");
+  },
+  computed: {
+    homeComp() {
+      return this.$store.state.homeComp;
     },
-    computed: {
-      homeComp() {
-        return this.$store.state.homeComp;
-      }
-    },
-    methods: {}
-  };
+    user() {
+      return this.$store.state.user;
+    }
+  },
+  methods: {}
+};
 </script>
 
 <style>
